f3934bc1c8408f975a8dc599261b9bc915ca1a3a
Lucia ALonso <lucia.alonso.duran@gmail.com>
2019-11-20 22:13:40 +0100

    creation testsScoreCalculateur

---
+++ src/test/java/deqo/ScoreCalculateurTest.java
@@ -0,0 +1,51 @@
+package deqo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ScoreCalculateurTest {
+
+    private QuestionAChoixMultiple questionAChoixMultiple;
+    private ScoreCalculateur scoreC;
+
+    @Before
+    public void setUp() {
+        questionAChoixMultiple = new QuestionAChoixMultiple("q1",new ArrayList<>(Arrays.asList(2,3,5)));
+        scoreC = new ScoreCalculateur();
+    }
+
+    @Test
+    public void calculeScore1() {
+        //GIVEN
+        float score;
+        //WHEN
+        score = scoreC.calculeScore(new ArrayList<>(Arrays.asList(1,4)), questionAChoixMultiple);
+        //THEN
+        assertEquals(score, 0f, 0f);
+    }
+
+    @Test
+    public void calculeScore2() {
+        //GIVEN
+        float score;
+        //WHEN
+        score = scoreC.calculeScore(new ArrayList<>(Arrays.asList(2,3)), questionAChoixMultiple);
+        //THEN
+        assertEquals(score, 2*100/3f, 0.01f);
+    }
+
+    @Test
+    public void calculeScore3() {
+        //GIVEN
+        float score;
+        //WHEN
+        score = scoreC.calculeScore(new ArrayList<>(Arrays.asList(2,3,5)), questionAChoixMultiple);
+        //THEN
+        assertEquals(score, 100f, 0.01f);
+    }
+}
